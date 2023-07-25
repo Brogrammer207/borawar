093837dabdf967cf82c721d6fc170c622b2817b2
manish <manishprajapat207@gmail.com>
2023-07-25 08:23:50 +0530

    by manish date - 25-07-2023

---
+++ lib/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:borawar/signup_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+    Get.to(const SignUpScreen());
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Center(child: Image.asset("assets/images/borawarlogo.png",height: 250,))],
+      ),
+    );
+  }
+}
